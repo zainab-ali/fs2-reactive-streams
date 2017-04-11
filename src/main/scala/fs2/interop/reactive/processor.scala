99dff88b746565853ce72d21799de66350748d33
zainab-ali <zainab.ali.london@gmail.com>
2017-04-12 00:32:13 +0100

    Increase logging in processor

--- src/main/scala/fs2/interop/reactive/processor.scala
+++ src/main/scala/fs2/interop/reactive/processor.scala
@@ -10,14 +10,29 @@ import fs2.async.mutable._
 import org.{reactivestreams => rs }
 import com.typesafe.scalalogging.LazyLogging
 
-final class Processor[A, B](p: Pipe[Task, A, B])(implicit AA: Async[Task]) extends rs.Processor[A, B] {
+final class Processor[A, B](p: Pipe[Task, A, B])(implicit AA: Async[Task]) extends rs.Processor[A, B] with LazyLogging {
 
   val sub = subscriber[A]().unsafeRun
   val pub = toUnicastPublisher[B](sub.stream through p)
 
-  def subscribe(subscriber: rs.Subscriber[_ >: B]): Unit = pub.subscribe(subscriber)
-  def onSubscribe(s: rs.Subscription): Unit = sub.onSubscribe(s)
-  def onNext(a: A): Unit = sub.onNext(a)
-  def onComplete(): Unit = sub.onComplete()
-  def onError(t: Throwable): Unit = sub.onError(t)
+  def subscribe(subscriber: rs.Subscriber[_ >: B]): Unit = {
+    logger.info(s"$this is has received subscriber $subscriber with $pub")
+    pub.subscribe(subscriber)
+  }
+  def onSubscribe(s: rs.Subscription): Unit = {
+    logger.info(s"$this has received subscription $s forwarding to $sub")
+    sub.onSubscribe(s)
+  }
+  def onNext(a: A): Unit = {
+    logger.info(s"$this has received element $a forwarding to $sub")
+    sub.onNext(a)
+  }
+  def onComplete(): Unit = {
+    logger.info(s"$this has completed")
+    sub.onComplete()
+  }
+  def onError(t: Throwable): Unit = {
+    logger.info(s"$this has received error $t")
+    sub.onError(t)
+  }
 }
